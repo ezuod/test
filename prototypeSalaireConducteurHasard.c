2f1ff807084ee1e9caf864bb49822d224d3f6ebd
ezuod <98274483+ezuod@users.noreply.github.com>
2022-02-15 14:40:57 +0100

    Update prototypeSalaireConducteurHasard.c

--- prototypeSalaireConducteurHasard.c
+++ prototypeSalaireConducteurHasard.c
@@ -2,59 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-//char nom = "";
+
 float revenu_au_hasard() {
     float j=0;
     j=(rand()%20);
     return j/10.0;
 }
 
-char nom_au_hasard() {
-    srand(time(NULL));
-    switch(rand()%10) {
-        case 1:{
-            nom = "Michel";
-            return "Michel";
-        } break;
-        case 2:{
-            nom = "Josué";
-            return "Josué";
-        } break;
-        case 3:{
-            nom = "Anguerrande";
-            return "Anguerrande";
-        } break;
-        case 4:{
-            nom = "Josselin";
-            return "Josselin";
-        } break;
-        case 5:{
-            nom = "Foulque";
-            return "Foulque";
-        } break;
-        case 6:{
-            nom = "Frederic";
-            return "Frederic";
-        } break;
-        case 7:{
-            nom = "David";
-            return "David";
-        } break;
-        case 8:{
-            nom = "Jean-Gertrude";
-            return "Jean-Gertrude";
-        } break;
-        case 9:{
-            nom = "Jépadnom";
-            return "Jépadnom";
-        } break;
-    }
-    return 1;
+char* nom_au_hasard() {
+    char* noms[10] = {"Michel","Josué","Anguerrande","Josselin","Foulque","Frederic","David","Jean-Gertrude","Jépadnom"}; 
+    return noms[rand()%9+0];
 }
 
 int main() {
     srand(time(NULL));
-    for (int i; i <= 50; i++) {
-    printf("le revenu sera de %f euro par kilomètre pour c ou c\n",revenu_au_hasard());};
+    for (int i=0; i <= 50; i++) {
+        char* nom = nom_au_hasard();
+        printf("le revenu sera de %f euro par kilomètre pour %s \n",revenu_au_hasard(),nom);
+    };
     return 0;
 }
